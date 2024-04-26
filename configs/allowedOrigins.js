5f3b971df2c2da8e647a90330f4a946da4abe326
Vulpes231 <whykayshot@gmail.com>
2024-04-26 11:31:14 +0100

    update list of allowed orgins

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:6000"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:6000",
+  "https://main--melodic-fenglisu-81161d.netlify.app",
+];
 
 module.exports = { allowedOrigins };
