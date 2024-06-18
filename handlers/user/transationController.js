c7c7e650ba1448c349395865365cc14b815010ad
Vulpes231 <whykayshot@gmail.com>
2024-06-18 21:45:11 +0100

    trnx

--- handlers/user/transationController.js
+++ handlers/user/transationController.js
@@ -1,9 +1,12 @@
 const Transaction = require("../../models/Transaction");
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 const getUserTransactions = async (req, res) => {
   const userId = req.userId;
+  const uid = new ObjectId(userId);
   try {
-    const trnx = await Transaction.find({ creator: userId });
+    const trnx = await Transaction.find({ creator: uid });
     res.status(200).json({ trnx });
   } catch (error) {
     console.log(error);
