7e382822a049cc385ce0100ac5a2a1c4d610b246
Vulpes231 <whykayshot@gmail.com>
2024-06-25 23:37:12 +0100

    updated cors

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:6000",
   "https://main--melodic-fenglisu-81161d.netlify.app",
+  "https://rabbit-admin-panel.netlify.app",
 ];
 
 module.exports = { allowedOrigins };
