27f0d484852982149b031cbc44c00108bd480d88
Vulpes231 <whykayshot@gmail.com>
2024-09-09 11:34:40 +0100

    edit user

--- handlers/user/userController.js
+++ handlers/user/userController.js
@@ -1,17 +1,13 @@
 const User = require("../../models/User");
-// Wallet
 const bcrypt = require("bcryptjs");
-const Wallet = require("../../models/Wallet");
 
 const getUser = async (req, res) => {
   const userId = req.userId;
-  // console.log("fired", userId);
   try {
     const user = await User.findOne({ _id: userId });
-    const wallet = await Wallet.findOne({ owner: userId });
 
     if (!user) return res.status(404).json({ message: "user not found!" });
-    return res.status(200).json({ user, wallet });
+    return res.status(200).json({ user });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "An error occured" });
@@ -19,13 +15,8 @@ const getUser = async (req, res) => {
 };
 
 const editUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, newPass } = req.body;
   const userId = req.userId;
-
-  // Validate input
-  if (!userId) {
-    return res.status(400).json({ message: "User ID is required." });
-  }
 
   try {
     // Find the user
@@ -41,8 +32,12 @@ const editUser = async (req, res) => {
 
     // Update user password if provided
     if (password) {
-      // Hash the new password
-      const hashedPass = await bcrypt.hash(password, 10);
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch)
+        return res.status(400).json({ message: "Incorrect password" });
+
+      const hashedPass = await bcrypt.hash(newPass, 10);
       user.password = hashedPass;
     }
 
@@ -50,7 +45,7 @@ const editUser = async (req, res) => {
     await user.save();
 
     // Send response
-    return res.status(200).json({ user });
+    return res.status(200).json({ message: "User updated successfully" });
   } catch (error) {
     console.error("Error updating user:", error);
     res
